847c22229d1248b913fb710055004121654148ef
real-yj <2000572@sit.singaporetech.edu.sg>
2021-10-24 22:55:35 +0800

    prototype 2 using libevtx

--- evtxIOC_prototype.py
+++ evtxIOC_prototype.py
@@ -1,10 +1,17 @@
-import Evtx.evtx as evtx
+import Evtx.Evtx as evtx
 import os
 from bs4 import BeautifulSoup
-
+import argparse
 
 if __name__ == "__main__":
-    with evtx.Evtx("/mnt/e/Desktop/Security.evtx") as log:
+    # argparse
+    help_msg = "python evtxIOC_prototype.py -f /dir/file"
+    parser = argparse.ArgumentParser(description=help_msg, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-f', '--file', type=str, required=True, help="/dir/file.evtx")
+    
+    args = parser.parse_args()
+
+    with evtx.Evtx(args.file) as log:
         for record in log.records():
             soup = BeautifulSoup(record.xml(), 'xml')
             if (soup.EventID.string == "4688"): print(soup)
@@ -12,5 +19,4 @@ if __name__ == "__main__":
             if (soup.EventID.string == "4672"): print(soup)
             if (soup.EventID.string == "1006"): print(soup)
             if (soup.EventID.string == "1007"): print(soup)
-            
-        
+            if (soup.EventID.string == "11"): print(soup)
