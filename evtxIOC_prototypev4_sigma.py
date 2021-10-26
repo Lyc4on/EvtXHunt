a58dc29817f2e1ec311d52ff35875cd9080089b6
nicholaschongwb@gmail.com <nicholaschongwb@gmail.com>
2021-10-26 19:21:41 +0800

    nic WIP

--- evtxIOC_prototypev4_sigma.py
+++ evtxIOC_prototypev4_sigma.py
@@ -1,13 +1,12 @@
 import os
 import pyevtx
-import Evtx.Evtx as evtx
 import argparse
 import csv
 import yaml
 import xmltodict, json
 import pandas as pd
 from pandas.io.json import json_normalize
-import re
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
     cols = [
@@ -43,47 +42,36 @@ if __name__ == "__main__":
     # Getting all fields in Evtx event entry
     for i in range(0, evtx_file.get_number_of_records()):
         # event Entry has 2 components: System and EventData
+        record_dict = {}
+        
         evtx_record = evtx_file.get_record(i) # evtx_record = event entry
-        evtx_record = xmltodict.parse(evtx_record.get_xml_string())
+        # evtx_record = xmltodict.parse(evtx_record.get_xml_string())
 
+        evtx_record = evtx_record.get_xml_string()
         # Get all System's sub elements
-        systemData = {}
-        for i in range(0, len(evtx_record['Event']['System'].values())):
-            # Check if System[i] is a dict or just a single element
-            if isinstance(evtx_record['Event']['System'].values()[i], dict):
-                if len(evtx_record['Event']['System'].values()[i].values()) > 1:
-                    print(evtx_record['Event']['System'].values()[i].keys()[0])
-                    print(evtx_record['Event']['System'].values()[i].values()[0])
-                    print(evtx_record['Event']['System'].values()[i].keys()[1])
-                    print(evtx_record['Event']['System'].values()[i].values()[1])
-                else:
-                    print(evtx_record['Event']['System'].values()[i].keys()[0])
-                    print(evtx_record['Event']['System'].values()[i].values()[0])
-            else:
-                print(evtx_record['Event']['System'].keys()[i])
-                print(evtx_record['Event']['System'].values()[i])
+        soup = BeautifulSoup(evtx_record, 'xml')
+        
+        provName = soup.System.Provider['Name']
+        provGUID = soup.System.Provider['Guid']
+        eventID = soup.System.EventID
+        version = soup.System.Version
+        level = soup.System.Level
+        task = soup.System.Task
+        opcode = soup.System.Opcode
+        keywords = soup.System.Keywords
+        timeCreated = soup.System.TimeCreated['SystemTime']
+        eventRecID = soup.System.EventRecordID
+        correlation = soup.System.Correlation
+        execPID = soup.System.Execution['ProcessID']
+        execTID = soup.System.Execution['ThreadID']
+        channel = soup.System.Channel
+        computer = soup.System.Computer
+        securityUID = soup.System.Security['UserID']
 
-        # print(systemData)
-        print("\n")
-
-        # Fix cmd output -> {u'4760': u'6844'} || JSON ->Execution : {@ProcessID: 4760, @ThreadID: 6844}
-        # print(evtx_record['Event']['System'].values()[10].keys()[0]) # Prints out @ProcessID
-
-        # Get all EventData's sub elements, every event entry in evtx has diff len of EventData, hence a loop is used
-        eventData = {}
-        for i in range(0, len(evtx_record['Event']['EventData']['Data'])):
-            key = evtx_record['Event']['EventData']['Data'][i].values()[0]
-
-            # Check if evtx_record['Event']['EventData']['Data'][i].values() has @Name and #text
-            if len(evtx_record['Event']['EventData']['Data'][i].values()) > 1:
-                val = evtx_record['Event']['EventData']['Data'][i].values()[1]
-                eventData[key] = val
-            else:
-                eventData[key] = ""
-        # print(eventData)
-
+        # eventData_arr = soup.EventData.findAll('Data')
+        print(soup.EventData.Data.string)
+        # print(eventData_arr)
         # Colidate variables into a dictionary before inserting to datafrace
-
 
     # Parsing and normalizing yml data to json
     with open(args.rule) as r:
